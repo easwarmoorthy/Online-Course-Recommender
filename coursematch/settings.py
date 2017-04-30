3089d9b2e5bac87dd226f05980c784e3b6fdff61
Easwar Moorthy <easwarvem@gmail.com>
2017-04-30 22:53:07 +0530

    Add allowed hosts

--- coursematch/settings.py
+++ coursematch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^oiwne34y*=e@&cowb_r58f1!11wlpdm!hdtlgf9yphp#_on(-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coursematch01.herokuapp.com','127.0.0.1']
 
 
 # Application definition
